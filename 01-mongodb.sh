a9c4ad781b397bbe587ae8016857058177094d3d
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-28 14:48:02 +0100

    api doc added

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -14,16 +14,16 @@ N="\e[0m"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
-   echo -e $TIMESTAMP [ERROR]  $R "Please run this script with root access $N" | tee -a $LOGS_FILE
-   exit 1
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
-      
+
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e $TIMESTAMP [ERROR] $2 ...$R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e $TIMESTAMP [INFO] $2 ...$G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
@@ -31,6 +31,15 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>> $LOGS_FILE
-VALIDATE $? "Installing MongoDB"  
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable --now mongod
+VALIDATE $? "Starting and enabling MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarting MongoDB"        "  
 
   
