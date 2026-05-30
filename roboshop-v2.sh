9ff22c62e1b439db591f060c466fbdaf3c3c037a
sowmyataraka <sowmya.gundaji@gmail.com>
2026-05-30 22:00:35 +0100

    api doc added

---
+++ roboshop-v2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z0184746WRRFG0I0XJTP"# replace with your zone ID
+DOMAIN_NAME="daws90s.uk" # replace with your domain name
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+### Validation ###
+if [ $# -lt 2 ]; then
+    echo -e "$R ERROR:: Atleast 2 arguments required $N"
+    echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
+    exit 1
+fi
+
+ACTION=$1
+shift # first argument will be removed
+
+if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete" ]; then
+    echo -e "$R ERROR:: First argument must be either create or delete $N"
+    echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
+    exit 1
+fi
+
