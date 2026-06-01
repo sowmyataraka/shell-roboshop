ea43eba99674d734d8a516e5feab4a5f09ad4502
sowmyataraka <sowmya.gundaji@gmail.com>
2026-06-01 10:50:41 +0100

    api doc added

--- 08-shipping.sh
+++ 08-shipping.sh
@@ -6,6 +6,7 @@ sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.daws90s.shop
 
 USERID=$(id -u)
 R="\e[31m"
@@ -29,9 +30,9 @@ VALIDATE(){
 }
 
 dnf install maven -y &>>$LOGS_FILE
-VALIDATE $? "Installing maven"
+VALIDATE $? "Installing Maven"
 
-d roboshop &>>$LOGS_FILE
+id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
     VALIDATE $? "Creating roboshop system user"
@@ -50,16 +51,29 @@ VALIDATE $? "Creating app directory"
 
 curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip  &>>$LOGS_FILE
 cd /app 
-unzip /tmp/shipping.zip &>>$LOGSFILE
+unzip /tmp/shipping.zip &>>$LOGS_FILE
 VALIDATE $? "Downloaded and extracted shipping code"
 
-mvn clean package &>>$LOGS_FILE
+mvn clean package  &>>$LOGS_FILE
+mv target/shipping-1.0.jar shipping.jar 
 VALIDATE $? "Installing dependencies"
 
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
 VALIDATE $? "Created systemctl service"
 
 dnf install mysql -y &>>$LOGS_FILE
-VALIDATE $? "Installed MySQL client"
+VALIDATE $? "Installing MySQL client"
 
+mysql -h $MYSQL_HOST -u root -pRoboShop@1 -e "use cities" &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+    VALIDATE $? "Data loaded"
+else
+    echo -e "Data already loaded ... $Y SKIPPING $N"
+fi
 
+systemctl enable shipping 
+systemctl restart shipping
+VALIDATE $? "Enable and restarted shipping"
