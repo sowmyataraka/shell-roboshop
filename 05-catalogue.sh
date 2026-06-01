4c3ebf1594bf10c0e2123d6e2c4dce55ca96668a
sowmyataraka <sowmya.gundaji@gmail.com>
2026-06-01 14:02:24 +0100

    api doc added

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -67,7 +67,7 @@ VALIDATE $? "Added Mongo repo"
 dnf install mongodb-mongosh -y &>>$LOGS_FILE
 VALIDATE $? "Installed MongoDB client"
 
-INDEX=$(mongosh --host mongodb.daws90s.uk-eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+INDEX=$(mongosh --host mongodb.daws90s.uk --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
 if [ $INDEX -lt 0 ]; then
     mongosh --host mongodb.daws90s.uk app/db/master-data.js &>>$LOGS_FILE
